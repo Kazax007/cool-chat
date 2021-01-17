25c5958fef7ba173d9188b5f2bae2d1f4e81ab4c
Kazax007 <badabum0001@gmail.com>
2021-01-17 16:13:24 +0300

    initial

---
+++ app/src/main/java/ru/kazax/chat/ui/login/LoginFormState.kt
@@ -0,0 +1,11 @@
+package ru.kazax.chat.ui.login
+
+/**
+ * Data validation state of the Login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val emailError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
